d08bcb61bacb1f34c60715bc3fd178cfbcaac74c
martijnhaex <martijn.haex@c4j.be>
2014-09-22 17:08:26 +0200

    Remove class TestData.

--- jgmanagement-git-api/src/main/java/be/haexnet/jgmanagement/git/model/Project.java
+++ jgmanagement-git-api/src/main/java/be/haexnet/jgmanagement/git/model/Project.java
@@ -20,4 +20,25 @@ public class Project {
     public String getDirectoryLocation() {
         return directoryLocation;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Project project = (Project) o;
+
+        if (directoryLocation != null ? !directoryLocation.equals(project.directoryLocation) : project.directoryLocation != null)
+            return false;
+        if (name != null ? !name.equals(project.name) : project.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (directoryLocation != null ? directoryLocation.hashCode() : 0);
+        return result;
+    }
 }
--- jgmanagement-git-api/src/test/java/be/haexnet/jgmanagement/git/GitManagerIntegrationTest.java
+++ jgmanagement-git-api/src/test/java/be/haexnet/jgmanagement/git/GitManagerIntegrationTest.java
@@ -1,17 +1,19 @@
 package be.haexnet.jgmanagement.git;
 
+import be.haexnet.jgmanagement.git.fixture.ProjectFixture;
 import be.haexnet.jgmanagement.git.model.Branch;
-import be.haexnet.jgmanagement.git.model.TestData;
+import be.haexnet.jgmanagement.git.model.Project;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.extractProperty;
 
 public class GitManagerIntegrationTest {
+    static final List<Project> PROJECTS = ProjectFixture.LIST();
+
     GitManager manager = new GitManager();
 
     @Test(expected = AssertionError.class)
@@ -21,26 +23,23 @@ public class GitManagerIntegrationTest {
 
     @Test
     public void remoteBranchesWithProjectsNoBaseBranch() throws Exception {
-        final List<Branch> branches = manager.withProjects(Arrays.asList(TestData.SSH.getProject(), TestData.GIT_JIRA.getProject()))
-                .remoteBranches();
+        final List<Branch> branches = manager.withProjects(PROJECTS).remoteBranches();
 
         assertThat(branches).hasSize(1);
         assertThat(extractProperty("name").from(branches)).containsOnly("feature-other-project");
         assertThat(extractProperty("lastCommit").from(branches)).containsOnly(new DateTime(2014, 9, 22, 9, 44, 57));
         assertThat(extractProperty("merged").from(branches)).containsOnly(false);
-        assertThat(extractProperty("project").from(branches)).containsOnly(TestData.SSH.getProject());
+        assertThat(extractProperty("project").from(branches)).containsOnly(ProjectFixture.SSH());
     }
 
     @Test
     public void remoteBranchesWithProjectsAndBaseBranch() throws Exception {
-        final List<Branch> branches = manager.withProjects(Arrays.asList(TestData.SSH.getProject(), TestData.GIT_JIRA.getProject()))
-                .withBaseBranch("origin/master")
-                .remoteBranches();
+        final List<Branch> branches = manager.withProjects(PROJECTS).withBaseBranch("origin/master").remoteBranches();
 
         assertThat(branches).hasSize(1);
         assertThat(extractProperty("name").from(branches)).containsOnly("feature-other-project");
         assertThat(extractProperty("lastCommit").from(branches)).containsOnly(new DateTime(2014, 9, 22, 9, 44, 57));
         assertThat(extractProperty("merged").from(branches)).containsOnly(false);
-        assertThat(extractProperty("project").from(branches)).containsOnly(TestData.SSH.getProject());
+        assertThat(extractProperty("project").from(branches)).containsOnly(ProjectFixture.SSH());
     }
 }
